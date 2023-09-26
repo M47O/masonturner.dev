381fab874cc9e9bb92666efe0b6fe9ac5a6dc539
Mason Turner <turner_m@lynchburg.edu>
2023-09-26 16:51:16 -0400

    Add preliminary not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,16 @@
 import Link from "next/link"
-
+import styles from "./not-found.module.css"
 export default function NotFound(){
     return (
-        <p>Error 404: This route does not exist. <Link href="/">Click here to navigate home</Link>.</p>
+        <div className={styles.errorContainer}>
+            <p className={styles.error}><span className={styles.errorNumber}>Error 404:</span> This route does not exist.</p>
+            <Link href="/"className={styles.buttonContainer}>
+                <div className={`${styles.corner} ${styles.cornerTopLeft}`} />
+                <div className={`${styles.corner} ${styles.cornerTopRight}`} />
+                <div className={`${styles.corner} ${styles.cornerBottomRight}`} />
+                <div className={`${styles.corner} ${styles.cornerBottomLeft}`} />
+                <span className={styles.homeButton}>Navigate Home</span>
+            </Link>
+        </div>
     )
 }
