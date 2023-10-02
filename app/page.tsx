71a58d31ff3f0e352607dfee012d450c3cd5489e
Mason Turner <turner_m@lynchburg.edu>
2023-10-02 10:24:08 -0400

    Add feed, feed styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,22 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <ul className={styles.list}>
-        <li><Link href="/about" className={styles.link}>about me</Link></li>
-        <li><Link href="/projects" className={styles.link}>projects</Link></li>
-        <li><a href="https://github.com/M47O" className={styles.link}>github</a></li>
-        <li><Link href="/settings" className={styles.link}>settings</Link></li>
+        <li className={styles.about}><Link href="/about" className={`${styles.link}`}>about me</Link></li>
+        <li className={styles.projects}><Link href="/projects" className={styles.link}>projects</Link></li>
+        <li className={styles.github}><a href="https://github.com/M47O" className={styles.link}>github</a></li>
+        <li className={styles.settings}><Link href="/" className={styles.link}>settings<span className={styles.comingsoon}> /* coming soon */</span></Link> </li>
+        <div className={styles.feedContainer}>
+					<div className={`${styles.feed} ${styles.feedAbout}`} />
+					<div className={`${styles.feed} ${styles.feedProjects}`} />
+					<div className={`${styles.feed} ${styles.feedGithub}`} />
+					<div className={`${styles.feed} ${styles.feedSettings}`} />
+          <span className={styles.standBy}>
+            <div className={`${styles.corner} ${styles.cornerTopLeft}`} />
+            <div className={`${styles.corner} ${styles.cornerBottomRight}`} />
+            Stand By
+            </span>
+					<div className={styles.feedOverlay} />
+			</div>
       </ul>
     </div>
   )
