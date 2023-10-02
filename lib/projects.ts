80d601b180129aad215383019915653e463299ed
Mason Turner <turner_m@lynchburg.edu>
2023-10-02 10:19:48 -0400

    Modify src path, add masonturner.dev

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,4 +1,4 @@
-export type Technologies =
+export type Technology =
   | "cloudinary"
   | "css"
   | "express"
@@ -18,26 +18,26 @@ export type Technologies =
   | "vercel"
   | "vite";
 
-export type Categories = 
+export type Category = 
   | "fullstack"
   | "frontend"
   | "backend"
   | "misc";
 
 export type ProjectProps = {
-  category: Categories,
-  src?: `${string}.webp`
+  category: Category,
+  src?: `/projects/${string}.webp`
   site: `https://${string}`;
   repo: `https://github.com/M47O/${string}`;
   title: string;
   description: string;
-  technologies: Technologies[];
+  technologies: Technology[];
 };
 
 export const projects: ProjectProps[] = [
   {
     category: "fullstack",
-    src: "quickdream.webp",
+    src: "/projects/quickdream.webp",
     site: "https://quickdream.netlify.app",
     title: "Quickdream",
     repo: "https://github.com/M47O/quickdream",
@@ -55,24 +55,24 @@ export const projects: ProjectProps[] = [
     description:
       "A full-stack social media application I created as an easy-to-use and more accessible alternative to Midjourney for AI image generation, as Midjourney no longer offers free image generation and is only accessible via Discord's platform."
   },
-  // {
-  //   category: "frontend",
-  //   src: "masonturnerdev.webp",
-  //   site: "https://masonturner.dev",
-  //   title: "masonturner.dev",
-  //   repo: "https://github.com/M47O/masonturner.dev",
-  //   technologies: [
-  //     "react",
-  //     "typescript",
-  //     "nextjs",
-  //     "vercel"
-  //   ],
-  //   description:
-  //     "A portfolio site (this one, in fact) created as a visual update to my old portfolio. I created this to play around with some new-to-me technologies and new specifications of CSS."
-  // },
   {
     category: "frontend",
-    src: "oldportfolio.webp",
+    src: "/projects/masonturnerdev.webp",
+    site: "https://masonturner.dev",
+    title: "masonturner.dev",
+    repo: "https://github.com/M47O/masonturner.dev",
+    technologies: [
+      "react",
+      "typescript",
+      "nextjs",
+      "vercel"
+    ],
+    description:
+      "A portfolio site (this one, in fact) created as a visual update to my old portfolio. I created this to play around with some new-to-me technologies and new specifications of CSS."
+  },
+  {
+    category: "frontend",
+    src: "/projects/oldportfolio.webp",
     site: "https://m47o.github.io/personal-portfolio/",
     title: "Old Portfolio",
     repo:"https://github.com/M47O/personal-portfolio",
@@ -85,7 +85,7 @@ export const projects: ProjectProps[] = [
   },
   {
     category: "misc",
-    src: "calculator.webp",
+    src: "/projects/calculator.webp",
     site: "https://m47o.github.io/calculator/",
     title: "Calculator",
     repo: "https://github.com/M47O/calculator",
@@ -97,7 +97,7 @@ export const projects: ProjectProps[] = [
   },
   {
     category: "misc",
-    src: "tenzies.webp",
+    src: "/projects/tenzies.webp",
     site: "https://m47o.github.io/tenzies/",
     title: "Tenzies",
     repo: "https://github.com/M47O/tenzies",
@@ -107,6 +107,6 @@ export const projects: ProjectProps[] = [
       "javascript",
       "css"
     ],
-    description: "A modern and responsive calculator built with TypeScript. Features a user-friendly interface with intuitive functionality. Take a second or two and read the documentation (and the footnotes) for it. I think this project sealed my position as one of the best doc writers in the industry."
+    description: "This is my take on Bob Ziroll's Tenzies, a dice game commonly played by children."
   }
 ]
